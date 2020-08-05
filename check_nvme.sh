75dda9a537cae4ba75983fa43d1bff8b95286a6b
Alexander Jackson <alexanderjackson@users.noreply.github.com>
2020-08-05 17:31:12 +0200

    Execute with sudo

--- check_nvme.sh
+++ check_nvme.sh
@@ -15,21 +15,21 @@ MESSAGE=""
 
 for DISK in $DISKS ; do
   # Check for critical_warning
-  $(nvme smart-log /dev/$DISK | awk 'FNR == 2 && $3 != 0 {exit 1}')
+  $(sudo nvme smart-log /dev/$DISK | awk 'FNR == 2 && $3 != 0 {exit 1}')
   if [ $? == 1 ]; then
     CRIT=true
     MESSAGE="$MESSAGE $DISK has critical warning "
   fi
 
   # Check media_errors
-  $(nvme smart-log /dev/$DISK | awk 'FNR == 15 && $3 != 0 {exit 1}')
+  $(sudo nvme smart-log /dev/$DISK | awk 'FNR == 15 && $3 != 0 {exit 1}')
   if [ $? == 1 ]; then
     CRIT=true
     MESSAGE="$MESSAGE $DISK has media errors "
   fi
 
   # Check num_err_log_entries
-  $(nvme smart-log /dev/$DISK | awk 'FNR == 16 && $3 != 0 {exit 1}')
+  $(sudo nvme smart-log /dev/$DISK | awk 'FNR == 16 && $3 != 0 {exit 1}')
   if [ $? == 1 ]; then
     CRIT=true
     MESSAGE="$MESSAGE $DISK has errors logged "
@@ -42,4 +42,3 @@ if [ $CRIT = "true" ]; then
 else
   echo "OK $(echo $DISKS | tr -d '\n')"
   exit 0
-fi
